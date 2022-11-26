c6fb3770f8824504170f2913e89e9402d5caca94
Ivan <felhat@bk.ru>
2022-11-26 15:26:32 +0300

    new

---
+++ bot_telega.py
@@ -0,0 +1,13 @@
+import telebot
+# Создаем экземпляр бота
+bot = telebot.TeleBot('5755251840:AAFnnRMIamisPkuoyqxJCzI23M7lLdibbcY')
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
